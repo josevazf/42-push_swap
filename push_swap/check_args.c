724b5dc08409369d34f1a8f875e8947055d1e12f
jrocha-v <jrocha-v@student.42porto.com>
2023-07-26 15:55:53 +0100

    checkargs

--- push_swap/check_args.c
+++ push_swap/check_args.c
@@ -6,46 +6,11 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:43:05 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/26 15:47:10 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/26 15:53:37 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	is_digit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-int	is_sign(char c)
-{
-	return (c == '+' || c == '-');
-}
-
-int	intcmp(const char *s1, const char *s2)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = i;
-	if (s1[i] == '+')
-	{
-		if (s2[j] != '+')
-			i++;
-	}
-	else
-	{
-		if (s2[j] == '+')
-			j++;
-	}
-	while (s1[i] != '\0' && s2[j] != '\0' && s1[i] == s2[j])
-	{
-		i++;
-		j++;
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
-}
 
 // Checks for dups
 int	is_duplicate(char **argv)
@@ -83,6 +48,7 @@ int	is_zero(char *argv)
 	return (1);
 }
 
+// Check if argument is a number and only has one sign
 int	is_number(char *argv)
 {
 	int i;
