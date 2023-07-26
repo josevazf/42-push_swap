8c8768d0bcb908bbfe73ba7e8a7b41704fb5e279
jrocha-v <jrocha-v@student.42porto.com>
2023-07-26 16:01:02 +0100

    push.h

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:49:31 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/26 15:48:37 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/26 16:00:16 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ typedef	struct	s_stack
 
 
 // check_args.c
-int check_args(char **argv);
-int	is_number(char *argv);
-int	is_sign(char c);
-int	is_digit(char c);
-int	is_duplicate(char **argv);
-int	intcmp(const char *s1, const char *s2);
+int 	check_args(char **argv);
+int		is_number(char *argv);
+int		is_sign(char c);
+int		is_digit(char c);
+int		is_duplicate(char **argv);
+int		intcmp(const char *s1, const char *s2);
 
-void	exit_error();
+void	error_handle();
 
 #endif
