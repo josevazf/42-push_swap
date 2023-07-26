65c2ef5a8548fe06bbdf6e4992a4303b5fd2b012
jrocha-v <jrocha-v@student.42porto.com>
2023-07-26 15:49:03 +0100

    check_args

--- push_swap/check_args.c
+++ push_swap/check_args.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:43:05 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/26 14:43:16 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/26 15:47:10 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,67 @@ int	is_sign(char c)
 	return (c == '+' || c == '-');
 }
 
+int	intcmp(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i;
+	if (s1[i] == '+')
+	{
+		if (s2[j] != '+')
+			i++;
+	}
+	else
+	{
+		if (s2[j] == '+')
+			j++;
+	}
+	while (s1[i] != '\0' && s2[j] != '\0' && s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
+}
+
+// Checks for dups
+int	is_duplicate(char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 1;
+		while (argv[j])
+		{
+			if (j != i && intcmp(argv[i], argv[j]) == 0)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+// Checks if argument is 0
+int	is_zero(char *argv)
+{
+	int i;
+
+	i = 0;
+	if (is_sign(argv[i]))
+		i++;
+	while (argv[i] && argv[i] == '0')
+		i++;
+	if (argv[i] != '\0')
+		return (0);
+	return (1);
+}
+
 int	is_number(char *argv)
 {
 	int i;
@@ -31,7 +92,7 @@ int	is_number(char *argv)
 		i++;
 	while (argv[i] && is_digit(argv[i]))
 		i++;
-	if (argv[i] != '/0' && !is_digit(argv[i]))
+	if (argv[i] != '\0' && !is_digit(argv[i]))
 		return (0);
 	return (1);	
 }
