b3735c2bed7ae31302866385d31c037935202d52
jrocha-v <jrocha-v@student.42porto.com>
2023-07-27 12:05:26 +0100

    push.h

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:49:31 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/27 11:44:20 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/27 12:02:55 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ typedef	struct		s_stack
 
 
 // check_args.c
-int 	check_args(char **argv);
+int 	check_args(char **argv/* , t_stack **stack_a */);
 int		is_number(char *argv);
 int		is_sign(char c);
 int		is_digit(char c);
@@ -35,8 +35,14 @@ int		is_duplicate(char **argv);
 int		intcmp(const char *s1, const char *s2);
 
 // ft_atoi.c
-int	ft_atoi(const char *str);
-int	ft_istdelimiter(char c);
+int		ft_atoi(const char *str);
+int		ft_istdelimiter(char c);
+
+// list_utils.c
+t_stack	*ft_create_node(int value);
+void    ft_print_list(t_stack *list);
+void	ft_stack_push_back(t_stack **begin_stack, int value);
+
 
 void	error_handle();
 
