44759fb99ebc64a9b416651ec94694e8ae9dd3ef
jrocha-v <jrocha-v@student.42porto.com>
2023-07-27 16:00:08 +0100

    ft_atoi

--- push_swap/ft_atoi.c
+++ push_swap/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:43:04 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/27 11:43:25 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/27 14:57:21 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,19 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 		n = n * 10 + str[i++] - '0';
 	return (n * sign);
+}
+
+int	ft_tobinary(long nbr)
+{
+	long posi;
+	
+	posi = 0;
+	if (nbr < 2)
+		posi = "01"[nbr];
+	if (nbr >= 2)
+	{	
+		ft_tobinary(nbr / 2);
+		ft_tobinary(nbr % 2);
+	}
+	return (posi);
 }
