41d6d7d87dd7c6bf1fa153927c45a6db53c9cbf5
jrocha-v <jrocha-v@student.42porto.com>
2023-07-27 11:45:52 +0100

    push.h

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:49:31 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/26 16:00:16 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/27 11:44:20 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-typedef	struct	s_stack 
+typedef	struct		s_stack 
 {
-	int value;
-	struct s_stack *next;
-	struct s_stack *prev;
+	int 			index;
+	int				bin_index;
+	int				value;
+	struct s_stack 	*next;
 }	t_stack;
 
 
@@ -32,6 +33,10 @@ int		is_sign(char c);
 int		is_digit(char c);
 int		is_duplicate(char **argv);
 int		intcmp(const char *s1, const char *s2);
+
+// ft_atoi.c
+int	ft_atoi(const char *str);
+int	ft_istdelimiter(char c);
 
 void	error_handle();
 
