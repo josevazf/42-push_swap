644031237bc24a5ebf83299d138554f3e690c266
jrocha-v <jrocha-v@student.42porto.com>
2023-07-27 11:45:19 +0100

    atoi

---
+++ push_swap/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 11:43:04 by jrocha-v          #+#    #+#             */
+/*   Updated: 2023/07/27 11:43:25 by jrocha-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_istdelimiter(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int i = 0;
+	int n = 0;
+	int sign = 1;
+
+	while (ft_istdelimiter(str[i]) && str[i])
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{	
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		n = n * 10 + str[i++] - '0';
+	return (n * sign);
+}
