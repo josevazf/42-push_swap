0e2099aacbc6119c360fb0d58047c04fe2e13152
jrocha-v <jrocha-v@student.42porto.com>
2023-07-28 21:34:33 +0100

    shadow_sort

--- push_swap/shadow_sort.c
+++ push_swap/shadow_sort.c
@@ -6,13 +6,13 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 11:44:45 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/07/28 12:00:06 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/07/28 12:16:11 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*get_min(t_stack *stack_a, int list_length)
+/* t_stack	*get_min(t_stack *stack_a, int list_length)
 {
 	t_stack	*current_node;
 	t_stack *min_node;
@@ -30,12 +30,28 @@ t_stack	*get_min(t_stack *stack_a, int list_length)
 		if (current_node->next)
 			current_node = current_node->next;
 	}
-	return (min_node);	
+	return (min_node);
+} */
+
+t_stack	*get_min(t_stack *stack_a, int list_length)
+{
+	t_stack	*current_node;
+	t_stack *min_node;
+	
+	current_node = stack_a;
+	min_node = current_node;
+	while (list_length--)
+	{
+		if (current_node->value < min_node->value && current_node->index != 0)
+			min_node = current_node;
+		if (current_node->next)
+			current_node = current_node->next;
+	}
+	return (min_node);
 }
 
 void	shadow_sort(t_stack **stack_a, int list_length)
 {
-	t_stack *current_node;
 	t_stack *min_node;
 	int 	index_value;
 
@@ -45,5 +61,6 @@ void	shadow_sort(t_stack **stack_a, int list_length)
 		min_node = get_min(*stack_a, list_length);
 		min_node->index = index_value;
 		index_value++;
+		ft_print_stack(*stack_a);
 	}
 }
