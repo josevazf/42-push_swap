7bf751a606680fefb17a84841459f429b1b378fc
jrocha-v <jrocha-v@student.42porto.com>
2023-08-01 13:17:41 +0100

    --

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,14 +6,14 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:52:40 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/08/01 12:01:06 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/08/01 13:12:47 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 //	Checks if the input is correct, if it is initializes stacks a and b,
-//	assigns each value indexes and sorts the stacks. When sorting is done, frees
+//	assigns each value and sorted indexes. When sorting is done, frees
 //	the stacks and exits.
 int	main(int argc, char **argv)
 {
@@ -27,32 +27,18 @@ int	main(int argc, char **argv)
 	stack_b = NULL;
 	stack_a = create_stack_a(argv, &stack_a);
 	shadow_sort(&stack_a, argc - 1);
-	//print_stack(stack_b);
-	//sa(&stack_a);
-	
-	printf("-- STACK A--\n");
-	print_stack(stack_a);
-	printf("-- STACK B --\n");
-	print_stack(stack_b);
-	
+		printf("-- STACK A--\n");
+		print_stack(stack_a);
+		printf("-- STACK B --\n");
+		print_stack(stack_b);
+		
+	//main_sort(&stack_a, &stack_b, argc - 1);
 	pb(&stack_b, &stack_a);
-	pb(&stack_b, &stack_a);
-	pb(&stack_b, &stack_a);
-	
-	printf("-- STACK A--\n");
-	print_stack(stack_a);
-	printf("-- STACK B --\n");
-	print_stack(stack_b);
-	//pa(&stack_a, &stack_b);
-	rrr(&stack_a, &stack_b);
-	//rb(&stack_b);
-
-
-	printf("-- STACK A--\n");
-	print_stack(stack_a);
-	printf("-- STACK B --\n");
-	print_stack(stack_b);
-	
+		printf("-- STACK A--\n");
+		print_stack(stack_a);
+		printf("-- STACK B --\n");
+		print_stack(stack_b);
+		
 	free_stack(&stack_a);
 	free_stack(&stack_b);
 	return (0);
