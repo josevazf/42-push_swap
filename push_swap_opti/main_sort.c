6ff4658d19e73b1b6eb7f2bbfeafaca9e4e224d3
jrocha-v <jrocha-v@student.42porto.com>
2023-08-02 14:43:07 +0100

    main_sort

--- push_swap_opti/main_sort.c
+++ push_swap_opti/main_sort.c
@@ -6,11 +6,24 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 12:23:07 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/08/01 16:16:16 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/08/02 12:37:30 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int		check_bit(t_stack *stack_a, int bit)
+{
+	t_stack *stack;
+	stack = stack_a;
+	while (stack->next)
+	{
+		if (((stack->index >> bit) & 1) == 0)
+			return (1);
+		stack = stack->next;
+	}
+	return (0);
+}
 
 int		is_sorted(t_stack *stack)
 {
@@ -29,24 +42,24 @@ void	main_sort(t_stack **stack_a, t_stack **stack_b, int list_length)
 {
 	int 	bit;
 	int 	size;
+	int 	trigger;
 	
 	bit = 0;
+	trigger = 0;
 	while (!is_sorted(*stack_a))
 	{
 		size = list_length;
-		while (size--)
+		while ((size-- && !is_sorted(*stack_a)) || check_bit(*stack_a, bit))
 		{
+/* 			if (!check_bit(*stack_a, bit))
+				printf("Break\n"); */
 			if (((((*stack_a)->index) >> bit) & 1) == 0)
-			{
 				pb(stack_b, stack_a);
-			}	
 			else
 				ra(stack_a);
 		}
-		while (*stack_b != NULL)
-		{
+		while (*stack_b)
 			pa(stack_a, stack_b);
-		}
 		bit++;
 	}
 }
