32e993e40c397e87483272cb3ca9333bde91c258
jrocha-v <jrocha-v@student.42porto.com>
2023-08-08 17:28:22 +0100

    --

--- push_swap_opti/push_swap.h
+++ push_swap_opti/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:49:31 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/08/08 14:27:10 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/08/08 17:22:10 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,12 @@ int		intcmp(const char *s1, const char *s2);
 int		ft_atoi(const char *str);
 int		is_delimiter(char c);
 
-// stack_create.c
+// stack_utils.c
 t_stack	*create_stack_a(char **argv, t_stack **stack_a);
 t_stack	*create_node(int value, long index, t_stack *previous);
-void	print_stack(t_stack *list);
 void	stack_push_back(t_stack **begin_stack, int value, long index);
 t_stack	*stack_last(t_stack *begin_stack);
+void	free_stack(t_stack **stack);
 
 // shadow_sort.c
 void	shadow_sort(t_stack **stack_a, int list_length);
@@ -76,10 +76,10 @@ void	rrr(t_stack **stack_a, t_stack **stack_b);
 
 // main_utils.c
 void	error_handle(void);
-void	free_stack(t_stack **stack);
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 int		is_sorted(t_stack *stack_a);
+int		is_inverted(t_stack *stack_a);
 
 // main_sort.c
 void	main_sort(t_stack **stack_a, t_stack **stack_b, int stack_size);
